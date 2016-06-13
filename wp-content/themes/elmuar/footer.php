4c9d9397e140c9eb6e07a03391a62b3ccd4eecd5
joaquin reyes <hola@jrltt.net>
2016-06-13 20:49:52 +0200

    add config and elmuar theme

---
+++ wp-content/themes/elmuar/footer.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package Elisa_Murcia_Artengo
+ */
+
+?>
+
+	</div><!-- #content -->
+
+	<footer id="colophon" class="site-footer" role="contentinfo">
+		<div class="site-info">
+			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'elmuar' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'elmuar' ), 'WordPress' ); ?></a>
+			<span class="sep"> | </span>
+			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'elmuar' ), 'elmuar', '<a href="http://jrltt.net" rel="designer">jrltt</a>' ); ?>
+		</div><!-- .site-info -->
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>

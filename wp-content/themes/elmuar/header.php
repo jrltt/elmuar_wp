ef1046c2a9171feeffc24ef935be8057214bfb32
Joaquin Reyes Lettieri <hola@jrltt.net>
2016-07-22 17:16:24 +0200

    show menu temporary

--- wp-content/themes/elmuar/header.php
+++ wp-content/themes/elmuar/header.php
@@ -42,10 +42,10 @@
 			endif; ?>
 		</div><!-- .site-branding -->
 
-<!-- 		<nav id="site-navigation" class="main-navigation" role="navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php //esc_html_e( 'Primary Menu', 'elmuar' ); ?></button>
-			<?php //wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
-		</nav>--><!-- #site-navigation --> 
+		<nav id="site-navigation" class="main-navigation" role="navigation">
+			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?phpesc_html_e( 'Primary Menu', 'elmuar' ); ?></button>
+			<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
+		</nav><!-- #site-navigation -->
 	</header><!-- #masthead -->
 
 	<div id="content" class="site-content">
--- wp-content/themes/elmuar/template-parts/content-page.php
+++ wp-content/themes/elmuar/template-parts/content-page.php
@@ -11,7 +11,6 @@
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
-	<h1>publication oh yeah</h1>
 		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
 	</header><!-- .entry-header -->
 

19763a11f80fd04b61a4f2b109b7be0c03f1fd63
Joaquin Reyes Lettieri <hola@jrltt.net>
2016-10-05 23:14:48 +0200

    open by default on parcours elements, add styles and jshintrc

--- wp-content/themes/elmuar/js/behavior.js
+++ wp-content/themes/elmuar/js/behavior.js
@@ -11,16 +11,16 @@ console.log('hello bro!!1');
 //     else {
 //       console.log('Smaller!');
 //       var flkty = new Flickity( '#carousel', {
-// 		  // options
-// 		  cellAlign: 'left',
-// 		  contain: true
-// 		});
+//        // options
+//        cellAlign: 'left',
+//        contain: true
+//      });
 //     }
 // }, true);
 //
 (function($) {
   $('.menu-item a[href*="#"]:not([href="#"])').click(function() {
-  	console.log('hoal');
+    console.log('hoal');
     if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
       var target = $(this.hash);
       target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
@@ -33,3 +33,12 @@ console.log('hello bro!!1');
     }
   });
 })(jQuery);
+
+var body = document.querySelector('body');
+if (classie.hasClass(body, 'page-template-parcour')) {
+  var toggles = document.querySelector('div.tailor-toggles');
+  for (var i = 0; i <= 1; i++) {
+    classie.toggleClass(toggles.children[i].children[0], 'is-active');
+    classie.toggleClass(toggles.children[i].children[1], 'show--text');
+  }
+}
